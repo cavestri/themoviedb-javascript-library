2cf7498f38c2565c4f8ed13b91727dbdd61fe3ef
Franco Cavestri <cavestri@gmail.com>
2014-03-19 00:15:47 -0300

    Added some methods and small changes
    Added the methods for: changes, collections, companies, credits.
    Rename of some variables to use underscores

--- themoviedb.js
+++ themoviedb.js
@@ -2,10 +2,16 @@
 var theMovieDb = {};
 
 //Access configuration
-theMovieDb.apiKey = "api_key=3d197569c7f13f60d61a7d61d5c83427";
-theMovieDb.baseURI = "http://api.themoviedb.org/3/";
-theMovieDb.imagesURI = "http://image.tmdb.org/t/p/";
+theMovieDb.api_key = "api_key=3d197569c7f13f60d61a7d61d5c83427";
+theMovieDb.base_uri = "http://api.themoviedb.org/3/";
+theMovieDb.images_uri = "http://image.tmdb.org/t/p/";
 theMovieDb.timeout = 2000;
+theMovieDb.page = "page=";
+theMovieDb.start_date = "start_date=";
+theMovieDb.end_date = "end_date=";
+theMovieDb.language = "language=";
+theMovieDb.append_to_response = "append_to_response=";
+theMovieDb.include_image_language = "include_image_language="
 
 //Common methods
 theMovieDb.generateQuery = function (parameters) {
@@ -27,7 +33,7 @@ theMovieDb.configurations = {
     getConfiguration: function (success, error){
         var options = {
             method: "GET",
-            url: "configuration" + theMovieDb.generateQuery([theMovieDb.apiKey]),
+            url: "configuration" + theMovieDb.generateQuery([theMovieDb.api_key]),
             status: 200
         };
 
@@ -37,7 +43,7 @@ theMovieDb.configurations = {
 
 theMovieDb.images = {
     getImage: function (options) {
-        return theMovieDb.imagesURI + options.size + "/" + options.file;
+        return theMovieDb.images_uri + options.size + "/" + options.file;
     }
 }
 
@@ -45,21 +51,175 @@ theMovieDb.account = {};
 theMovieDb.authentication = {};
 
 theMovieDb.certifications = {
-    getList: function (sucess, error) {
+    getList: function (success, error) {
         theMovieDb.client(
             {
                 method: "GET",
                 status: 200,
-                url: "certification/movie/list" + theMovieDb.generateQuery([theMovieDb.apiKey])
+                url: "certification/movie/list" + theMovieDb.generateQuery([theMovieDb.api_key])
             }, 
-            sucess, error);
+            success, error);
     }
 };
 
-theMovieDb.changes = {};
-theMovieDb.collections = {};
-theMovieDb.companies = {};
-theMovieDb.credits = {};
+theMovieDb.changes = {
+    getMovieChanges: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.page) {
+            query.push(theMovieDb.page + options.page);
+        }
+        
+        if(options.start_date) {
+            query.push(theMovieDb.start_date + options.start_date);
+        }
+        
+        if(options.end_date) {
+            query.push(theMovieDb.end_date + options.end_date);
+        }
+        
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "movie/changes" + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    },
+    getPersonChanges: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.page) {
+            query.push(theMovieDb.page + options.page);
+        }
+        
+        if(options.start_date) {
+            query.push(theMovieDb.start_date + options.start_date);
+        }
+        
+        if(options.end_date) {
+            query.push(theMovieDb.end_date + options.end_date);
+        }
+        
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "person/changes" + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    }
+};
+
+
+theMovieDb.collections = {
+    getCollection: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.language) {
+            query.push(theMovieDb.language + options.language);
+        }
+            
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "collection/" + options.id + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    },
+    getCollectionImages: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.language) {
+            query.push(theMovieDb.language + options.language);
+        }
+        
+        if(options.imageLanguage) {
+            query.push(theMovieDb.include_image_language + options.imageLanguage);
+        }
+            
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "collection/" + options.id + "/images" + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    }
+    
+};
+
+
+theMovieDb.companies = {
+    getCompany: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.append_to_response) {
+            query.push(theMovieDb.append_to_response + options.append_to_response);
+        }
+            
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "company/" + options.id + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    },
+    getCompanyMovies: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.language) {
+            query.push(theMovieDb.language + options.language);
+        }
+        
+        if(options.append_to_response) {
+            query.push(theMovieDb.append_to_response + options.append_to_response);
+        }
+        
+        if(options.page) {
+            query.push(theMovieDb.page + options.page);
+        }
+            
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "company/" + options.id + "/movies" + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    }
+    
+};
+
+
+theMovieDb.credits = {
+    getCredit: function (options, success, error) {
+        
+        var query = [theMovieDb.api_key];
+        
+        if(options.language) {
+            query.push(theMovieDb.language + options.language);
+        }
+            
+        theMovieDb.client(
+            {
+                method: "GET",
+                status: 200,
+                url: "credit/" + options.id + theMovieDb.generateQuery(query)
+            },
+            success, error);
+    },
+};
+
+
 theMovieDb.discover = {};
 theMovieDb.find = {};
 
@@ -68,7 +228,7 @@ theMovieDb.genres = {
         var options = {
                 method: "GET",
                 status: 200,
-                url: "genre/list" + theMovieDb.generateQuery([theMovieDb.apiKey])
+                url: "genre/list" + theMovieDb.generateQuery([theMovieDb.api_key])
         };
 
         theMovieDb.client(options, succes, error);
@@ -95,7 +255,7 @@ theMovieDb.client = function (options, success, error){
         error("Timeout");
     };
     
-    xhr.open(options.method, theMovieDb.baseURI + options.url, true);
+    xhr.open(options.method, theMovieDb.base_uri + options.url, true);
         
     xhr.onload = function (e) {
         if(xhr.readyState === 4) {
